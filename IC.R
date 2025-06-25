e0118c4fd3dfcb830bdd9915c032f0160c907736
Andre Trovello <andre.a.trovello@gmail.com>
2025-06-25 18:37:02 -0300

    code filters lines

--- IC.R
+++ IC.R
@@ -29,7 +29,55 @@ drive_download(
   )
 
 
-txt <- read_lines(file_name)
-head(txt)
+data <- read.table(file_name, skip = 32, header = FALSE)
+
+#txt <- read_lines(file_name)
+print(data)
 
 #getwd()
+
+header <- readLines(file_name)[31:32]
+print(header)
+
+clean_text <- function(line) {
+    line <- unlist(strsplit(line, "\\s+"))
+    line <- line[line != "#"]
+
+}
+
+read_data <- function(file, skip_lines) {
+    data <- read.table(file, skip = skip_lines, header = FALSE)
+    header <- readLines(file)[skip_lines-1:skip_lines]
+
+    print(header)
+
+    idx <- clean_text(header[1])
+    unidade <- clean_text(header[2])
+
+    header_final <- paste(idx, unidade)
+    colnames(data) <- header_final
+    return(data)
+    
+}
+
+data <- read_data(file_name, 32)
+print(data)
+
+
+colnames(data) <- header_final
+
+print(data)
+
+
+idx <- clean_text(header[1])
+unidade <- clean_text(header[2])
+
+print(idx)
+print(unidade)
+
+header_final <- paste(idx, unidade)
+print(header_final)
+
+colnames(data) <- header_final
+
+print(data)
