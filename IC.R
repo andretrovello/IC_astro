063d1b9e57a62af6513d6424cc928d3cbaac339b
Andre Trovello <andre.a.trovello@gmail.com>
2025-06-25 20:19:09 -0300

    update

--- IC.R
+++ IC.R
@@ -6,16 +6,12 @@ library(googledrive)
 drive_auth()
 
 loc <- 'IC_Astro/Input/Models_July_2019/miles'
-
 folder <- drive_get(loc)
-
 content <- drive_ls(folder)
 #print(content)
 
 sed_files <- content %>%
     filter(str_ends(name, '_sed'))
-
-print(sed_files$name[1])
 
 if (!grepl('\\.txt$', sed_files$name[1])) {
     file_name <- paste0(sed_files$name[1], '.txt')
@@ -28,28 +24,21 @@ drive_download(
     overwrite = TRUE             # Sobrescreve se já existir
   )
 
-
-data <- read.table(file_name, skip = 32, header = FALSE)
-
 #txt <- read_lines(file_name)
-print(data)
-
 #getwd()
-
-header <- readLines(file_name)[31:32]
-print(header)
 
 clean_text <- function(line) {
     line <- unlist(strsplit(line, "\\s+"))
     line <- line[line != "#"]
+    return(line)
 
 }
 
 read_data <- function(file, skip_lines) {
-    data <- read.table(file, skip = skip_lines, header = FALSE)
-    header <- readLines(file)[skip_lines-1:skip_lines]
+    data <- read.table(file, skip = skip_lines+1, header = FALSE)
+    header <- readLines(file)[skip_lines:(skip_lines + 1)]
 
-    print(header)
+    print(header[2])
 
     idx <- clean_text(header[1])
     unidade <- clean_text(header[2])
@@ -60,24 +49,6 @@ read_data <- function(file, skip_lines) {
     
 }
 
-data <- read_data(file_name, 32)
+data <- read_data(file_name, 31)
 print(data)
 
-
-colnames(data) <- header_final
-
-print(data)
-
-
-idx <- clean_text(header[1])
-unidade <- clean_text(header[2])
-
-print(idx)
-print(unidade)
-
-header_final <- paste(idx, unidade)
-print(header_final)
-
-colnames(data) <- header_final
-
-print(data)
